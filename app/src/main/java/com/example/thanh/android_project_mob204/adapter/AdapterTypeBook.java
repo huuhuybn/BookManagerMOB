0ae726eb141c8d4fd4aafc23afac29f8331a81f2
MAC2015 <huynh@tonish.vn>
2018-09-28 11:08:46 +0700

    Happy COding

--- app/src/main/java/com/example/thanh/android_project_mob204/adapter/AdapterTypeBook.java
+++ app/src/main/java/com/example/thanh/android_project_mob204/adapter/AdapterTypeBook.java
@@ -1,11 +1,15 @@
 package com.example.thanh.android_project_mob204.adapter;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.thanh.android_project_mob204.R;
 import com.example.thanh.android_project_mob204.holder.HolderTypeBook;
@@ -37,12 +41,107 @@ public class AdapterTypeBook extends RecyclerView.Adapter<HolderTypeBook> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull HolderTypeBook holder, int position) {
+    public void onBindViewHolder(@NonNull HolderTypeBook holder, final int position) {
+        final TypeBook typeBook = typeBooks.get(position);
+        holder.tvID.setText(typeBook.id);
+        holder.tvName.setText(typeBook.name);
+
+
+        holder.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                long result = typeBookDAO.deleteTypeBook(typeBook.id);
+                if (result < 0) {
+                    Toast.makeText(context,"Co loi xay ra!!!",Toast.LENGTH_SHORT).show();
+                } else {
+                    // xoa du lieu tuong ung sau khi xoa trong DB
+                    typeBooks.remove(position);
+
+                    // f5 lai listview
+                    notifyDataSetChanged();
+
+                }
+
+            }
+        });
+        holder.btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.dialog_update_type_book);
+                 final EditText edtTypeName;
+                 final EditText edtDescription;
+                 final EditText edtPosition;
+                 Button btnUpdate;
+                 Button btnCancel;
+
+                edtTypeName = dialog.findViewById(R.id.edtTypeName);
+                edtDescription = dialog.findViewById(R.id.edtDescription);
+                edtPosition = dialog.findViewById(R.id.edtPosition);
+                btnUpdate = dialog.findViewById(R.id.btnUpdate);
+                btnCancel = dialog.findViewById(R.id.btnCancel);
+
+                edtTypeName.setText(typeBook.name);
+                edtDescription.setText(typeBook.description);
+                edtPosition.setText(typeBook.position);
+
+
+                btnUpdate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        TypeBook typeBook1 = new TypeBook();
+                        typeBook1.id = typeBook.id;
+
+                        String name = edtTypeName.getText().toString().trim();
+                        String des = edtDescription.getText().toString().trim();
+                        String pos = edtPosition.getText().toString().trim();
+
+                        typeBook1.name = name;
+                        typeBook1.description = des;
+                        typeBook1.position = pos;
+
+                        long result = typeBookDAO.updateTypeBook(typeBook1);
+                        if (result< 0){
+                            Toast.makeText(context,"Co loi xay ra!!!",Toast.LENGTH_SHORT).show();
+                        }else {
+
+                            typeBooks.get(position).name = name;
+                            typeBooks.get(position).description = des;
+                            typeBooks.get(position).position = pos;
+
+                            // f5 listview
+                            notifyDataSetChanged();
+
+                            dialog.dismiss();
+                        }
+
+
+                    }
+                });
+
+                btnCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+
+
+                dialog.show();
+
+
+
+            }
+        });
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return typeBooks.size();
     }
 }
--- app/src/main/java/com/example/thanh/android_project_mob204/fragment/LibraryFragment.java
+++ app/src/main/java/com/example/thanh/android_project_mob204/fragment/LibraryFragment.java
@@ -4,23 +4,65 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.thanh.android_project_mob204.R;
+import com.example.thanh.android_project_mob204.adapter.AdapterTypeBook;
+import com.example.thanh.android_project_mob204.database.DatabaseHelper;
+import com.example.thanh.android_project_mob204.sqlitedao.TypeBook;
+import com.example.thanh.android_project_mob204.sqlitedao.TypeBookDAO;
+
+import java.util.List;
+import java.util.Random;
 
 public class LibraryFragment extends Fragment {
 
 
     private RecyclerView lvTypeBooks;
 
+    private AdapterTypeBook adapterTypeBook;
+    private List<TypeBook> typeBooks;
+
+    private LinearLayoutManager linearLayoutManager;
+
+    private DatabaseHelper databaseHelper;
+    private TypeBookDAO typeBookDAO;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_library, container, false);
         lvTypeBooks = view.findViewById(R.id.lvTypeBooks);
+        databaseHelper = new DatabaseHelper(getActivity());
+        typeBookDAO = new TypeBookDAO(databaseHelper);
+
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+
+        typeBooks = typeBookDAO.getAllTypeBooks();
+
+        for (int i = 0; i < 20; i++) {
+
+            TypeBook typeBook = new TypeBook();
+            typeBook.id = i + new Random().nextInt() + "";
+            typeBook.name = "Android " + new Random().nextInt();
+            typeBook.description = "Hello Moto";
+            typeBook.position = i + "";
+            typeBooks.add(typeBook);
+
+        }
+
+        Log.e("SIZE" , typeBooks.size() + "");
+
+        adapterTypeBook = new AdapterTypeBook(getActivity(), typeBooks, typeBookDAO);
+
+        lvTypeBooks.setLayoutManager(linearLayoutManager);
+
+        lvTypeBooks.setAdapter(adapterTypeBook);
 
 
         return view;
