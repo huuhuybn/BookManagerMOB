eb3de4f92c69b390025c64fbdc0b6cbb23107435
MAC2015 <huynh@tonish.vn>
2018-09-28 10:05:36 +0700

    Happy COding

--- app/src/main/java/com/example/thanh/android_project_mob204/fragment/LibraryFragment.java
+++ app/src/main/java/com/example/thanh/android_project_mob204/fragment/LibraryFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +12,25 @@ import android.view.ViewGroup;
 import com.example.thanh.android_project_mob204.R;
 
 public class LibraryFragment extends Fragment {
+
+
+    private RecyclerView lvTypeBooks;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_library, container, false);
+        lvTypeBooks = view.findViewById(R.id.lvTypeBooks);
+
+
         return view;
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 }
